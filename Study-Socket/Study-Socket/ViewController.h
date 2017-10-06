33b81db71ee8f654f2c8103a0cec9f7a48673a5c
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-10-06 16:08:33 +0900

    iPhonepurojekutono be-su

---
+++ Study-Socket/Study-Socket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Study-Socket
+//
+//  Created by 清 貴幸 on 2017/10/06.
+//  Copyright © 2017年 daybysay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Study-Socket/Study-Socket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Study-Socket
+//
+//  Created by 清 貴幸 on 2017/10/06.
+//  Copyright © 2017年 daybysay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
