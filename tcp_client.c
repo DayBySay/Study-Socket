936719aa98c51fa9215fb4041659ba34da65456c
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-10-07 15:36:23 +0900

    ipとportを渡せるようにした

--- tcp_client.c
+++ tcp_client.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char *argv[]) {
   struct sockaddr_in server;   
   int socket0;          
   char buf[32];         
   int n;
 
+
   socket0 = socket(AF_INET, SOCK_STREAM, 0);  
 
   server.sin_family = AF_INET;           
-  server.sin_port = htons(10080);          
-  server.sin_addr.s_addr = inet_addr("127.0.0.1");  
+  server.sin_addr.s_addr = inet_addr(argv[1]);  
+  server.sin_port = htons(atoi(argv[2]));          
 
   connect(socket0, (struct sockaddr *)&server, sizeof(server)); 
 
