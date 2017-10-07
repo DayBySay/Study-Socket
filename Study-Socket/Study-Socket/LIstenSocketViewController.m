7d6d6f723ca183aa67f96bcb96db8164012377fa
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-10-07 15:26:06 +0900

    実機で待てるようにした

--- Study-Socket/Study-Socket/LIstenSocketViewController.m
+++ Study-Socket/Study-Socket/LIstenSocketViewController.m
@@ -11,6 +11,10 @@
 
 @interface LIstenSocketViewController () <GCDAsyncSocketDelegate>
 @property (nonatomic) GCDAsyncSocket *listenSocket;
+@property (nonatomic) GCDAsyncSocket *fromSocket;
+@property (nonatomic) GCDAsyncSocket *toSocket;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
 @end
 
 @implementation LIstenSocketViewController
@@ -24,7 +28,33 @@
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didAcceptNewSocket:(GCDAsyncSocket *)newSocket {
+    [self appendLog:NSStringFromSelector(_cmd)];
     NSLog(@"Accept:%p",sock);
+    [self appendLog:[NSString stringWithFormat:@"Accept:%p",sock]];
+    
+    NSError *error = nil;
+    GCDAsyncSocket *toSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    if (![toSocket connectToHost:@"www.google.com" onPort:80 error:&error]) {
+        NSLog(@"error: %@", error);
+    } else {
+        self.fromSocket = newSocket;
+        self.toSocket = toSocket;
+    }
 }
+
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port {
+    [self appendLog:NSStringFromSelector(_cmd)];
+}
+
+- (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err {
+    [self appendLog:NSStringFromSelector(_cmd)];
+}
+
+- (void)appendLog:(NSString *)log {
+    NSString *current = self.textView.text;
+    NSString *appended = [NSString stringWithFormat:@"🍜 %@\n%@", log, current];
+    self.textView.text = appended;
+}
+
 
 @end
