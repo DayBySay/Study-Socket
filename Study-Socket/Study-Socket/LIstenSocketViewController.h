fa6d091e7bad41350a6f87c1cfdc82af054ad405
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-10-07 14:26:52 +0900

    Listen用の実装追加

---
+++ Study-Socket/Study-Socket/LIstenSocketViewController.h
@@ -0,0 +1,13 @@
+//
+//  LIstenSocketViewController.h
+//  Study-Socket
+//
+//  Created by 清 貴幸 on 2017/10/07.
+//  Copyright © 2017年 daybysay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LIstenSocketViewController : UIViewController
+
+@end
---
+++ Study-Socket/Study-Socket/LIstenSocketViewController.m
@@ -0,0 +1,37 @@
+//
+//  LIstenSocketViewController.m
+//  Study-Socket
+//
+//  Created by 清 貴幸 on 2017/10/07.
+//  Copyright © 2017年 daybysay. All rights reserved.
+//
+
+#import "LIstenSocketViewController.h"
+
+@interface LIstenSocketViewController ()
+
+@end
+
+@implementation LIstenSocketViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
